3ff28c13055e8173ea23370600e0fd498f6a204e
Kalinka5 <106172806+Kalinka5@users.noreply.github.com>
2022-06-26 20:52:43 +0300

    Delete display.py

--- display.py
+++
@@ -1,5 +0,0 @@
-# create class AttrDisplay
-class AttrDisplay:
-    # function where print class of footballer and his name
-    def __repr__(self):
-        return f'[{self.__class__.__name__}: {getattr(self, "name")}]'
